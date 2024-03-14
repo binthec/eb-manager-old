7d9ef11cd79a15208c14b7a36a40378df5524409
binthec <binthec@gmail.com>
2024-03-14 21:11:47 +0900

    vue3を導入

---
+++ laravel/resources/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="page">
+        <p>{{ counter }}</p>
+        <button @click="counter += 1">
+            click!
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App.vue",
+
+    data () {
+        return {
+            counter: 0,
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
